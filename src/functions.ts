29c80423faa389109a800f5af553fc33a64cc6c0
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-02-08 12:01:55 +0100

    started on random shuffle function

--- src/functions.ts
+++ src/functions.ts
@@ -15,3 +15,21 @@ export const renderAllQuotes = () => {
   document.querySelector('#quotes')!.innerHTML = quote
   .map( q => `<li>${q.content}</li>`).join('')
 }
+
+/**
+ * Render random Quote
+ */
+export const renderRandomQuote = () => {
+ 
+}
+/**
+ * Fisher-Yates Shuffle functions
+ */
+const shuffle = (a: any) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const t = a[i]
+    a[i] = a[j]
+    a[j] = t
+  }
+}
