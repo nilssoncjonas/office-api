137721d1234c99c8dabcd5ffee4a259a9364a817
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-02-09 19:11:55 +0100

    fake slow api variable

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,16 @@ import { IQuotes } from "./interface";
 
 const BASE_URL = ' http://localhost:3001'
 const PATH = '/quotes'
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 1000;
 /**
  * Fetch quote from server
  */
 export const fetchQuotes = async () => {
   const res = await fetch(`${BASE_URL}${PATH}`)
+
+  FAKE_SLOW_API && await new Promise(r => setTimeout(r, FAKE_SLOW_API_DELAY));
+
   if (!res.ok) {
     throw new Error(`${res.status} ${res.statusText}`)
   }
